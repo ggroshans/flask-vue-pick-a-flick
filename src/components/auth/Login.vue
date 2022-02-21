b278b97fa2a23542e34d5ee6e34ecb7a6c1f8a63
ggroshansii <garthgroshansii@gmail.com>
2022-02-20 19:12:56 -0500

    integrate flask api with vue app

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,14 +1,29 @@
 <template>
     <div class="container">
         <h1>Book Match</h1>
+        <p>{{pTag}}</p>
     </div>
 </template>
 
 <script>
 /* eslint-disable */
     export default {
+    data() {
+        return {
+
+            pTag: null,
+            }
+            },
+    async created() {
         
+            const resp = await fetch("http://localhost:5000/greeting");
+            console.log('response', resp)
+            const obj = await resp.json();
+            console.log("obj", obj);
+            this.pTag = obj.greeting; 
+        }
     }
+    
 </script>
 
 <style scoped>
