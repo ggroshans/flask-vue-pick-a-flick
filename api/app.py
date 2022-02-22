b29ddd7ec9a6012a5235eaf3de04d6295ff9c35d
ggroshansii <garthgroshansii@gmail.com>
2022-02-22 16:41:58 -0500

    create database; create User db table

--- api/app.py
+++ api/app.py
@@ -1,8 +1,24 @@
+from enum import unique
+import os
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__)
 CORS(app)
+
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = 'secret_key'
+
+print(os.environ['HOME'])
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
 
 @app.route("/login")
@@ -10,8 +26,8 @@ def login():
     return {"test": "TEST"}
 
 
-@app.route("/register")
+@app.route("/register", ["POST"])
 def register():
+    
     return {"test": "TEST"}
 
-app.run(debug=True)
