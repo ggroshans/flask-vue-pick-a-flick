b8d0273d9e2e081ca55107caa4175b115d750f2e
ggroshansii <garthgroshansii@gmail.com>
2022-02-22 13:53:12 -0500

    create login form

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,33 +1,49 @@
 <template>
-    <div class="container">
-        <h1>Book Date</h1>
-        <p>{{pTag}}</p>
-    </div>
+  <div class="container">
+      <h1>Login</h1>
+    <form>
+      <div class="input-group form-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text"><b-icon icon="person-fill"></b-icon></span>
+        </div>
+        <input type="text" class="form-control" placeholder="username" />
+      </div>
+      <div class="input-group form-group">
+        <div class="input-group-prepend">
+    <span class="input-group-text"><b-icon icon="key-fill"></b-icon></span>
+        </div>
+        <input type="password" class="form-control" placeholder="password" />
+      </div>
+      <!-- <div class="row align-items-center remember">
+        <input type="checkbox" />Remember Me
+      </div> -->
+      <div class="form-group">
+        <input type="submit" value="Login" class="btn btn-primary float-right" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 /* eslint-disable */
-    export default {
-    data() {
-        return {
-
-            pTag: null,
-            }
-            },
-    async created() {
-        
-            const resp = await fetch("http://localhost:5000/greeting");
-            console.log('response', resp)
-            const obj = await resp.json();
-            console.log("obj", obj);
-            this.pTag = obj.test; 
-        }
-    }
-    
+export default {
+  data() {
+    return {
+      pTag: null
+    };
+  },
+  async created() {
+    const resp = await fetch("http://localhost:5000/greeting");
+    console.log("response", resp);
+    const obj = await resp.json();
+    console.log("obj", obj);
+    this.pTag = obj.test;
+  }
+};
 </script>
 
 <style scoped>
-    h1 {
-        font-family: 'Lobster Two';
-    }
+h1 {
+  font-family: "Lobster Two";
+}
 </style>
