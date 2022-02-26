28bdb1cd11e694525eff72a64506ff20cdabcf29
ggroshansii <garthgroshansii@gmail.com>
2022-02-26 12:38:40 -0500

    test request obj in app.py; create registration event that submits username/password that sends POST request to /register route

--- api/app.py
+++ api/app.py
@@ -1,16 +1,20 @@
 from enum import unique
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)                                   
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# SECRET_KEY = os.environ.get("SECRET_KEY")
 app.config['SECRET_KEY'] = 'secret_key'
 
 print(os.environ['HOME'])
@@ -28,6 +32,8 @@ def login():
 
 @app.route("/register", methods=["POST"])
 def register():
-    
+
+    print(request.get_json())
+    print(request.get_data())
     return {"test": "TEST"}
 
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -8,7 +8,7 @@
             ><b-icon icon="person-fill"></b-icon
           ></span>
         </div>
-        <input type="text" class="form-control" placeholder="username" />
+        <input type="text" class="form-control" placeholder="username" v-model="username"/>
       </div>
       <div class="input-group form-group">
         <div class="input-group-prepend">
@@ -16,15 +16,15 @@
             ><b-icon icon="key-fill"></b-icon
           ></span>
         </div>
-        <input type="password" class="form-control" placeholder="password" />
+        <input type="password" class="form-control" placeholder="password" v-model="password" />
       </div>
       <!-- <div class="row align-items-center remember">
         <input type="checkbox" />Remember Me
       </div> -->
       <div class="form-group">
         <input
+          @click="submitRegistration"
           type="submit"
-          value="Login"
           class="btn btn-primary float-right"
         />
       </div>
@@ -41,19 +41,30 @@
 <script>
 /* eslint-disable */
 export default {
+  data() {
+    return {
+      username: null,
+      password: null
+    };
+  },
+  methods: {
+    async submitRegistration() {
 
-  async created() {
+
     const resp = await fetch("http://localhost:5000/register", {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        'real-test': 'this is the real test'
+        "username": this.username,
+        "password": this.password
       })
     });
-    console.log(resp.json())
+    console.log(resp.json());
+    }
   }
+
 };
 </script>
 
