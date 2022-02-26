b675fc679bc3648d8dfc1cdc7f98c1231072aae3
ggroshansii <garthgroshansii@gmail.com>
2022-02-26 15:21:59 -0500

    add fetch request to /login route; add v-model to username/password inputs

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,28 +1,39 @@
 <template>
   <div class="container">
-      <h1>Login</h1>
+    <h1>Login</h1>
     <form>
       <div class="input-group form-group">
         <div class="input-group-prepend">
-          <span class="input-group-text"><b-icon icon="person-fill"></b-icon></span>
+          <span class="input-group-text"
+            ><b-icon icon="person-fill"></b-icon
+          ></span>
         </div>
-        <input type="text" class="form-control" placeholder="username" />
+        <input type="text" class="form-control" placeholder="username"  v-model="username" />
       </div>
       <div class="input-group form-group">
         <div class="input-group-prepend">
-    <span class="input-group-text"><b-icon icon="key-fill"></b-icon></span>
+          <span class="input-group-text"
+            ><b-icon icon="key-fill"></b-icon
+          ></span>
         </div>
-        <input type="password" class="form-control" placeholder="password" />
+        <input type="password" class="form-control" placeholder="password" v-model="password"/>
       </div>
       <!-- <div class="row align-items-center remember">
         <input type="checkbox" />Remember Me
       </div> -->
       <div class="form-group">
-        <input type="submit" value="Login" class="btn btn-primary float-right" />
+        <button
+          @click.prevent="submitLogin"
+          value="Login"
+          class="btn btn-primary float-right"
+        >Submit</button>
       </div>
     </form>
     <div>
-        <p>Don't have any account? <router-link to="/register">Register Here</router-link></p>
+      <p>
+        Don't have any account?
+        <router-link to="/register">Register Here</router-link>
+      </p>
     </div>
   </div>
 </template>
@@ -32,9 +43,25 @@
 export default {
   data() {
     return {
-      
+      username: null,
+      password: null
     };
   },
+      async submitLogin() {
+      console.log("fired")
+    const resp = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "username": this.username,
+        "password": this.password
+      })
+    });
+    this.username = "";
+    this.password = "";
+    }
 
 };
 </script>
