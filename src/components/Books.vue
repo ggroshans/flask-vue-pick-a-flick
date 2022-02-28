7e455138820e87a717a92be1d912f13faf8ac807
ggroshansii <garthgroshansii@gmail.com>
2022-02-28 15:01:46 -0500

    add swing and vue-swing packages; add vue-swing component to Books component

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -2,21 +2,30 @@
     <div>
         <!-- <router-link v-for="book in bookList" :key="book" :to="{name: 'Book', params: {book: book.list_name_encoded}}">{{category.display_name}}</router-link> -->
 
-        <ul v-for="book in bookList" :key="book.primary_isbn10"><li>{{book}}</li></ul>
+        <!-- <ul v-for="book in bookList" :key="book.primary_isbn10"><li>{{book}}</li></ul> -->
+        <vue-swing
+            v-for="book in bookList" :key="book.primary_isbn10" :config="config"><li>{{book}}</li></vue-swing>
     </div>
 </template>
 
 <script>
 /* eslint-disable */
-    import Category from './Book.vue'
+    import Category from './Book.vue';
+    import VueSwing from 'vue-swing';
     export default {
         data() {
             return {
                 bookList: null,
+                config: {
+                    allowedDirections:  [VueSwing.Direction.LEFT, VueSwing.Direction.RIGHT],
+                    minThrowDistance: 250,
+                    maxThrowDistance: 300
+                },
             }
         },
         components: {
             Category,
+            VueSwing,
         },
         async created() {
             const resp = await fetch("http://localhost:5000/books", {
