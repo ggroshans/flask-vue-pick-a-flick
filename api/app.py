7436ad527ae85dc1190772f9e26c19c2ec44d702
ggroshansii <garthgroshansii@gmail.com>
2022-03-14 17:30:48 -0400

    remove flask-login in lieu of flask_jwt_extended

--- api/app.py
+++ api/app.py
@@ -3,29 +3,25 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
 import bcrypt
 import requests
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
                            
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 SECRET_KEY = os.getenv("SECRET_KEY")
-db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = SECRET_KEY
+db = SQLAlchemy(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
+jwt = JWTManager(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
@@ -44,8 +40,8 @@ def login():
     
     if user:
         if bcrypt.checkpw(bytes(password, 'utf-8'), user.password):
-            login_user(user)
-            return jsonify({"success": "user access granted"})
+            access_token = create_access_token(identity=username);
+            return jsonify({"success": "user access granted", "token": access_token})
         else:
             return jsonify({"error": {"password" :"Passwords do not match"}})
     else:
@@ -71,9 +67,8 @@ def register():
 
 
 @app.route('/logout', methods=["GET", "POST"])
-@login_required
 def logout():
-    logout_user()
+    pass
 
 @app.route('/movies', methods=["POST"])
 def books():
