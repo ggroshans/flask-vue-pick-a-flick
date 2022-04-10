8b07a97c2193f7ab2a4bbda2a25eb1f33f4707d9
ggroshansii <garthgroshansii@gmail.com>
2022-04-10 11:06:09 -0400

    change m/movie route to not include /:genre id since its changing to more than one genre being selected

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ export default new Router({
       component: Register
     },
     {
-      path: '/movies/:genre',
+      path: '/movies/',
       name: 'Movies',
       component: Movies
     },
